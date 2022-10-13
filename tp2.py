33e074cdfbfad02ecfc254e34b448ce44957b261
NoahPfister <112487984+NoahPfister@users.noreply.github.com>
2022-10-13 10:23:21 -0400

    Update tp2.py

--- tp2.py
+++ tp2.py
@@ -3,17 +3,20 @@
 
 import random
 
+#ici je definit le maximum
 minimum = 1
-maximum = 5
+maximum = 1000
 
 
 
-
+#Ceci choisi quel nombre serai le maximum
 answer = random.randint(minimum, maximum)
 guessed = False
 
+#ceci est la fonction qui tient compte du nombre d'essai
 tries = 0
 
+#on demande pour le nombre requis, et si tu reussi, ca recommence.
 while not guessed:
     write = input("devin un nombre entre 1 et 1000")
     write = int(write)
@@ -26,7 +29,7 @@ while not guessed:
         if reesayer == "n":
            print("Merci et au revoir")
            guessed = True
-   
+#si tu ne reussis pas, ceci
     if answer < write:
         print("votre reponse est plus grande que la reponse?")
         tries = tries + 1
@@ -34,3 +37,5 @@ while not guessed:
     if answer > write:
         print("votre reponse est plus petite que la reponse?")
         tries = tries + 1
+
+        
